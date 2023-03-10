3d2d11905cddd5d2ccca279cb42ab383fc0f0d2c
brukyu <bbrunoigorr@gmail.com>
2023-03-09 21:53:15 -0300

    added index.js to Task

---
+++ src/pages/Task/index.js
@@ -0,0 +1,12 @@
+import react from "react"
+import { View, Text } from "reat-native"
+
+export default function Task() { 
+    return (
+        <view>
+            <Text>
+                Page Tasks
+            </Text>
+        </view>
+    )
+}
