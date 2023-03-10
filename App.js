eb62ba53e9ddf0eec22113cff7fdfb979531bee1
brukyu <bbrunoigorr@gmail.com>
2023-03-09 22:24:52 -0300

    modified App.js in .

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Task from "../src/pages/Task";
+import Task from "./src/pages/Task"
+import NewTask from "./src/pages/Task/NewTask";
+import Details from "./src/pages/Task/NewTask/Details";
 
 const Stack = createStackNavigator()
 
@@ -12,6 +15,23 @@ export default function App() {
         <Stack.Screen
           name="Task"
           component={Task}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
+        />
+        <Stack.Screen
+          name="NewTask"
+          component={NewTask}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
