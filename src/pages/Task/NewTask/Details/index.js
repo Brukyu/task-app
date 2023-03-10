3b470329ecd6e8cca8ce9bc7fff0deb7b344d53b
brukyu <bbrunoigorr@gmail.com>
2023-03-09 22:24:37 -0300

    modified index.js in Details

---
+++ src/pages/Task/NewTask/Details/index.js
@@ -0,0 +1,12 @@
+import react from "react"
+import { View, Text } from "react-native"
+
+export default function Details() { 
+    return (
+        <view>
+            <Text>
+                Page Details
+            </Text>
+        </view>
+    )
+}
