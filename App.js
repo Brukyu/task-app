68de41ef415a63fcff6ff57b915b65f078746955
brukyu <bbrunoigorr@gmail.com>
2023-03-09 21:51:45 -0300

    modified App.js in .

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Task from "../src/pages/Task";
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Task">
+        <Stack.Screen
+          name="Task"
+          component={Task}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
